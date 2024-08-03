a6e89be5caf64fd01721b3a1e2988c2e39d11104
salih damar <bilprogeem@gmail.com>
2024-08-03 21:13:17 +0300

    Model, Field, Pillow,Register,Media,Migrate

--- smartedu_con/smartedu/settings.py
+++ smartedu_con/smartedu/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
+    'courses.apps.CoursesConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,9 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#MEDIA FİLE
+
+MEDIA_URL= '/media/'
+MEDIA_ROOT= os.path.join(BASE_DIR,'media')
