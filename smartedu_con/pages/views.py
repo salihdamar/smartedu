42a4dce5f3e5d06242f0453dc76f6c0c17e38887
salih damar <bilprogeem@gmail.com>
2024-08-09 01:58:51 +0300

    FormView, ModalForm, Contact_Sayfası, CSRF, Messages

--- smartedu_con/pages/views.py
+++ smartedu_con/pages/views.py
@@ -2,6 +2,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from courses.models import Course
+from django.views.generic.edit import FormView
+from . forms import ContactForm
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
 
 class IndexView(TemplateView):
     template_name="index.html"
@@ -12,4 +16,14 @@ class IndexView(TemplateView):
        return context
 
 class AboutView(TemplateView):
-    template_name='about.html'    
+    template_name='about.html' 
+
+class ContactView( SuccessMessageMixin,FormView):
+    template_name='contact.html' 
+    form_class= ContactForm
+    success_url= reverse_lazy('contact')
+    success_message= 'We received your request'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
